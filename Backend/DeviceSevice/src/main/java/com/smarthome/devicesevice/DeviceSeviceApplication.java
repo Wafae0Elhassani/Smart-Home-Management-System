7a2bfb559b797d56127d88622440f951efa9332d
Wafae Elhassani <151550428+Wafae0Elhassani@users.noreply.github.com>
2024-07-20 20:01:46 +0100

    Add files via upload

---
+++ Backend/DeviceSevice/src/main/java/com/smarthome/devicesevice/DeviceSeviceApplication.java
@@ -0,0 +1,13 @@
+package com.smarthome.devicesevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceSeviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceSeviceApplication.class, args);
+	}
+
+}
